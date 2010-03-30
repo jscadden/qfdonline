1860d95c41833dbade36311e6a543ed2b821722d
Eric Wollesen <ericw@xmtp.net>
2010-03-30 15:13:24 -0600

    Missed these in the last commit: As a user I can create a new HOQ

--- config/routes.rb
+++ config/routes.rb
@@ -41,5 +41,9 @@ ActionController::Routing::Routes.draw do |map|
   #map.connect ':controller/:action/:id'
   #map.connect ':controller/:action/:id.:format'
 
-  map.resources :qfds
+  map.resources :qfds do |qfds|
+    qfds.resources :hoqs
+  end
+
+  
 end
