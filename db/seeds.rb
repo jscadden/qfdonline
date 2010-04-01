6123be6b2156c276ef1d28cd6dc5cb6d8722efe7
Eric Wollesen <ericw@xmtp.net>
2010-04-01 17:28:47 -0600

    New DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,26 @@
 #   Major.create(:name => 'Daley', :city => cities.first)
 
 
-user = User.create!(:login => "ericw",
-                    :email => "ericw@e3labs.com",
+user = User.create!(:login => "user",
+                    :email => "user@qfdonline.com",
                     :password => "password",
                     :password_confirmation => "password")
 
 qfd = user.qfds.create!(:name => "Test QFD")
 
 3.times do |x|
-  hoq = qfd.hoqs.create!(:name => "Test HOQ ##{x}")
+  hoq = Hoq.new(:name => "Test HOQ ##{x}")
+  qfd.hoq_list.insert_back(hoq)
 
   if x.zero?
     3.times do |y|
-      req = hoq.primary_requirements.create!(:name => "Test Requirement ##{Requirement.count}")
+      req = Requirement.new(:name => "Test Requirement ##{Requirement.count}")
+      hoq.primary_requirements_list.requirements << req
     end
   end
 
   3.times do |z|
-    req = hoq.secondary_requirements.create!(:name => "Test Requirement ##{Requirement.count}")
+      req = Requirement.new(:name => "Test Requirement ##{Requirement.count}")
+      hoq.secondary_requirements_list.requirements << req
   end
 end
