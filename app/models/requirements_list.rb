2e4874d86c6b104c91be6d9186cd6ee8f8aa07b2
Eric Wollesen <ericw@xmtp.net>
2010-04-03 01:40:45 -0600

    Make sure requirements stay in order

--- app/models/requirements_list.rb
+++ app/models/requirements_list.rb
@@ -1,6 +1,6 @@
 class RequirementsList < ActiveRecord::Base
   # essentially a many to many table
-  has_many :requirements
+  has_many :requirements, :order => "position"
 
   has_one :primary_hoq, :class_name => "Hoq", :foreign_key => "primary_requirements_list_id"
   has_one :secondary_hoq, :class_name => "Hoq", :foreign_key => "secondary_requirements_list_id"
