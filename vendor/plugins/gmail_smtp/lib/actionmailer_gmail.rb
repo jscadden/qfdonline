af351e2de54f35c1eac62f84790fe3860c5718ef
Eric Wollesen <ericw@xmtp.net>
2010-06-09 15:24:09 -0600

    Another attempt at getting mail configured to work in dev and prod

--- vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -1,9 +1,12 @@
-ActionMailer::Base.smtp_settings = {
-	:address => "smtp.gmail.com",
-	:port => 587,
-	:authentication => :plain,
-	:domain => ENV['GMAIL_SMTP_USER'],
-	:user_name => ENV['GMAIL_SMTP_USER'],
-	:password => ENV['GMAIL_SMTP_PASSWORD'],
-        :enable_starttls_auto => true,
-}
+settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :domain => ENV['GMAIL_SMTP_USER'],
+  :user_name => ENV['GMAIL_SMTP_USER'],
+  :password => ENV['GMAIL_SMTP_PASSWORD'],
+} 
+if "1.8.6" != VERSION
+  settings.merge!(:enable_starttls_auto => true)
+end
+ActionMailer::Base.smtp_settings = settings
