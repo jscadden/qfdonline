d88802626a80ed77986a903c9fa6e4a79a452de3
Eric Wollesen <ericw@xmtp.net>
2010-06-11 11:27:58 -0600

    Clean up the authorization rules a little

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,24 +1,25 @@
 authorization do
   role :user do
-    has_permission_on :qfds, :to => :alter do
+    has_permission_on :qfds, :to => :manage do
       if_attribute :user => is {user}
+    end
+
+    has_permission_on :qfds, :to => :collaborate do
       if_attribute :invitations => intersects_with {user.invitations_received}
     end
 
     has_permission_on :qfds, :to => :read do
       if_attribute :public => is {true}
     end
-
-    has_permission_on :qfds, :to => :create
-    has_permission_on :qfds, :to => [:delete, :destroy] do
-      if_attribute :user => is {user}
-    end
   end
 end
     
 privileges do
-  privilege :alter do
+  privilege :collaborate do
     includes :read, :update
   end
 
+  privilege :manage do
+    includes :create, :read, :update, :delete
+  end
 end
