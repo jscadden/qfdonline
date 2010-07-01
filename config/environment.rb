6b548f0c0ba43b1bad0a80b76d2938d2367308f4
Eric Wollesen <ericw@xmtp.net>
2010-07-01 17:05:41 -0600

    Add uservoice.  I don't think this is 100% functional yet, as the uservoice gem needs to support ERB before this will work with heroku.  I've submitted a pull request via github.

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "haml", :version => "< 3.0"
   config.gem "declarative_authorization"
   config.gem "spreadsheet"
+  config.gem "uservoice"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
